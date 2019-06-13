fd1173b1534a7523faf368422005f82475f62dc2
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-13 20:40:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,48 @@
 # My Code here....
+
+#map-like. All return an ARRAY
+def map_to_negativize(num)
+  arr = []
+  num.each do |i|
+  arr.push(i * -1)
+  end
+  arr
+end
+
+def map_to_no_change(arr)
+  arr
+end
+
+def map_to_double(arr)
+  arrret = []
+  arr.each do |i|
+  arrret.push(i * 2)
+  end
+arrret
+end
+
+def map_to_square(arr)
+  foo = []
+  arr.each do |i|
+    foo.push(i * i)
+  end
+  foo
+end
+
+#reduce-like. all return a VALUE
+
+def reduce_to_total(source_array, starting_point=0)
+  num = 0
+  source_array.length.times do |i|
+    num = (i * i) + starting_point
+  end
+  num
+end
+
+def reduce_to_all_true(arr)
+
+end
+
+def reduce_to_any_true(arr)
+
+end
